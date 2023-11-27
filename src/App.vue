5b161030217a447ff0f9fa1404bb56183b99a696
Mariano Vargas <marianoluisvargas@gmail.com>
2023-11-27 12:27:37 -0300

    fix: sintaxis de componentes arreglada

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <h1> Human Benchmark</h1>
-<button @click="start">Start new game</button>
+<button @click="start" :disabled="isPlaying">Start new game</button>
+<BlockTarget v-if="isPlaying" :delay="delay" />
 </template>
 
 <script setup>
+import BlockTarget from './components/BlockTarget.vue';
 import { ref } from 'vue';
 
 const isPlaying = ref(false);
@@ -12,7 +14,6 @@ const delay = ref(null);
 function start() {
   delay.value = 2000 + Math.random() * 5000;
   isPlaying.value = true;
-  console.log('delay', delay.value);
 }
 
 
@@ -24,7 +25,7 @@ function start() {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/BlockTarget.vue
@@ -0,0 +1,38 @@
+<template>
+<div v-if="isShowing" class="block">
+    click me
+</div>
+</template>
+
+<script setup>
+import { defineProps, onMounted, ref} from 'vue';
+
+const isShowing = ref(false);
+
+const props = defineProps({
+    delay: {
+        type: Number,
+    }
+})
+
+onMounted(() => {
+    setTimeout(() => {
+        isShowing.value = true;
+    }, props.delay);
+});
+
+console.log('delay', props.delay);
+
+</script>
+
+<style scoped>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background-color: crimson;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
